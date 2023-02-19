834617e500c3146827596e7008c0e5ffd1f9465b
Test Neo <testneo03@gmail.com>
2023-02-19 12:09:11 +0530

    begining

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '1/2': '50.0%'
+      }
+    },
+  },
+  plugins: [],
+}
